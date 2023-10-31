8299d778701c56e15ea884df9c040e795a72e4f7
Jeremy Fenech <jeremyfenech03@gmail.com>
2023-10-31 14:30:31 +0100

    working chat with bing ai

--- chat.py
+++ chat.py
@@ -6,17 +6,26 @@ import g4f
 # print(g4f.Provider.Ails.params)  # supported args
 
 # Automatic selection of provider
+messages = [{"role": "system", "content": "Make quick short replies with general details. You are Medi-Bot a medical chatbot with the purpose of informing the user on whether the illness they are experiencing requires any antibiotics. Do not introduce yourself unless a user asks. You should also ask follow up questions to narrow down the solution, such as age and severity of issue."}]
+
+messages = [{"role": "system", "content": "I want you to act like a medical chatbot named Medi-bot. The chatbot will ask questions about the illness which a user may be having and will then inform the user on whether they need antibiotics or not. The chatbot will also ask follow up questions to narrow down the solution, such as age and severity of issue."}]
 
 # streamed completion
-response = g4f.ChatCompletion.create(
-    model=g4f.models.gpt_4,
-    messages=[{"role": "user", "content": "Hello"}],
-    stream=True,
-    provider=g4f.Provider.Bing,
-)
+while True:
+    question = input("\n[USER]: ")
+    messages.append({"role": "user", "content": question})
 
-for message in response:
-    print(message, flush=True, end='')
+    response = g4f.ChatCompletion.create(
+        model=g4f.models.gpt_4,
+        messages=messages,
+        stream=True,
+        provider=g4f.Provider.Bing,
+    )
+
+    print("\n[MEDI-BOT]: ", end='')
+    for message in response:
+        print(message, flush=True, end='')
+
 
 # # normal response
 # response = g4f.ChatCompletion.create(
--- static/script.js
+++ static/script.js
@@ -23,3 +23,59 @@ $(document).ready(function () {
     ); // Adjust the animation duration as needed
   }
 });
+
+// chatbot
+function getBotResponse() {
+  var rawText = $("#textInput").val();
+  var userHtml = '<p class="userText"><span>' + rawText + "</span></p>";
+  $("#textInput").val("");
+  $("#chatbox").append(userHtml);
+
+  // Create the initial bot HTML elements
+  var botParagraph = $('<p class="botText"></p>');
+  var botSpan = $("<span></span>");
+
+  // Append the bot span to the bot paragraph
+  botParagraph.append(botSpan);
+
+  // Append the bot paragraph to the chatbox
+  $("#chatbox").append(botParagraph);
+
+  // Use the Fetch API to handle the streaming content
+  fetch("/get?msg=" + rawText)
+    .then((response) => response.body.getReader())
+    .then((reader) => {
+      const processStream = async () => {
+        while (true) {
+          const { done, value } = await reader.read();
+
+          if (done) {
+            // If the stream is done, break out of the loop
+            break;
+          }
+
+          // Decode the value as UTF-8
+          const decodedValue = new TextDecoder("utf-8").decode(value);
+
+          // Set the decoded value as text content of the bot span
+          botSpan.text(botSpan.text() + decodedValue);
+        }
+      };
+
+      // Start processing the stream
+      processStream();
+    });
+}
+
+$("#textInput").keypress(function (e) {
+  if (e.which == 13 && document.getElementById("textInput").value != "") {
+    getBotResponse();
+  }
+});
+
+$("#buttonInput").click(function () {
+  console.log("button clicked");
+  if (document.getElementById("textInput").value != "") {
+    getBotResponse();
+  }
+});
